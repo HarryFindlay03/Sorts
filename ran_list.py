15187c29021651f336a24e9459ae455f1513d31a
Harry Findlay <46387503+HarryFindlay03@users.noreply.github.com>
2020-05-30 23:31:58 +0100

    Update ran_list.py

--- ran_list.py
+++ ran_list.py
@@ -3,6 +3,7 @@ import random
 n = int(input("Enter Length of List: "))
 numbers = [random.randint(0, 1000000) for x in range(0, n)]
 
+# Clears the file of any numbers that are already in it
 f = open('numbers.txt', 'r+')
 f.truncate(0)
 f.close()
