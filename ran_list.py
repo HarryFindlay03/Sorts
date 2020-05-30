d9e6dd8a878ddff29619c2d46875a1f05a11e936
Harry Findlay <46387503+HarryFindlay03@users.noreply.github.com>
2020-05-30 23:30:14 +0100

    Add files via upload

--- ran_list.py
+++ ran_list.py
@@ -3,6 +3,11 @@ import random
 n = int(input("Enter Length of List: "))
 numbers = [random.randint(0, 1000000) for x in range(0, n)]
 
+f = open('numbers.txt', 'r+')
+f.truncate(0)
+f.close()
+
 with open('numbers.txt', 'w') as file:
 	for item in numbers:
 		file.write("%s, " % item)
+file.close()
