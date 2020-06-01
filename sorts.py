e90647682be6bad02585332378f8d3ba9a3ae8d7
HarryFindlay <harryfindlay@outlook.com>
2020-06-01 14:40:36 +0100

    Adding Quick Sort

--- sorts.py
+++ sorts.py
@@ -68,26 +68,57 @@ class Sorts:
 		total = t1 - t0
 		tup = (arr, total)
 		return(tup)
+	
+	### QUICK SORT
+
+	def partition(self, arr, start, end):
+		i = (start - 1) # Index of smaller element
+		pivot = arr[end]
+
+		for j in range(start, end):
+			if arr[j] <= pivot:
+				i = i + 1
+				temp = arr[i]
+				arr[i] = arr[j]
+				arr[j] = temp
+		
+		temp = arr[i + 1]
+		arr[i + 1] = arr[end]
+		arr[end] = temp
+		return(i + 1)
+
+	def quickSort(self, arr, start, end):
+		t0 = time.time()
+		if start < end:
+			# pi is partitioning index, arr[p] is now at right place
+			pi = self.partition(arr, start, end)
+
+			#Seperately sort elements before and after partition
+
+			self.quickSort(arr, start, pi - 1)
+			self.quickSort(arr, pi + 1, end)
+		t1 = time.time()
+		total = t1 - t0
+		tup = (arr, total)
+		return(tup)
+
 
 def menu(choice, arr):
 	x = Sorts(arr)
 	# Returned as a tuple [0] = the sorted array, [1] = the time taken
-	if choice == 1:
+	if choice == 1: # Bubble
+		print("With List Length of: ", len(numbers))
 		print("Time Taken: ", x.bubble()[1])
-	elif choice == 2:
+	elif choice == 2: # Insetion
+		print("With List Length of: ", len(numbers))
 		print("Time Taken: ", x.insertion()[1])
-	elif choice == 3:
+	elif choice == 3: # Merge
+		print("With List Length of: ", len(numbers))
 		print("Time Taken: ", x.mergeSort(numbers)[1])
-	elif choice == 4:
-		bubbleTime = x.bubble()[1]
-		insertionTime = x.insertion()[1]
-		if insertionTime < bubbleTime:
-			print("With list length: ", len(numbers), "...")
-			print("The insertion sort was faster than the bubble sort by: ", (bubbleTime - insertionTime), "seconds")
-		elif bubbleTime < insertionTime:
-			print("With list length: ", len(numbers, "..."))
-			print("The bubble sort was faster than the insertion sort by: ", (insertionTime - insertionTime), "seconds")
-
+	elif choice == 4:  # Quick
+		print("With List Length of: ", len(numbers))
+		print("Time Taken: ", x.quickSort(numbers, 0, len(numbers) - 1)[1])
+	
 
 file = open('numbers.txt', 'r')
 
@@ -96,7 +127,7 @@ for line in file:
 
 numbers = lineList
 
-choice = int(input("(1) Bubble    (2) Insetion    (3) Merge : "))
+choice = int(input("(1) Bubble    (2) Insetion    (3) Merge    (4) Quick : "))
 
 menu(choice, numbers)
 
